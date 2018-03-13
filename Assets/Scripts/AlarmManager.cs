aaeab8d44c81ddb2be116b4709474baf5719f314
chupawidth <chupawidth@gmail.com>
2018-03-13 17:38:48 +0700

    first commit

---
+++ Assets/Scripts/AlarmManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Timers;
+
+public class AlarmManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+
+
+}
---
+++ Assets/Scripts/ApplicationManager.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using LockingPolicy = Thalmic.Myo.LockingPolicy;
+using Pose = Thalmic.Myo.Pose;
+using UnlockType = Thalmic.Myo.UnlockType;
+using VibrationType = Thalmic.Myo.VibrationType;
+using System;
+
+public class ApplicationManager : SingletonBehaviour<ApplicationManager>
+{
+
+	#region PublicParameter
+    public GameObject myo = null;
+    public InputField countdownInputField;
+	#endregion
+
+	#region PrivateParameter
+
+	private bool objCreate = false;
+    ThalmicMyo thalmicMyo;
+    bool isTimeUp = false;
+    float countDown = 1f;
+    #endregion
+
+	#region LifeCycle
+
+	// Use this for initialization
+	void Start ()
+	{
+		if (!objCreate) {
+			objCreate = true;
+			_instance = this;
+			DontDestroyOnLoad (instance);
+            thalmicMyo = myo.GetComponent<ThalmicMyo>();
+
+		} else {
+			DestroyImmediate (gameObject);
+		}
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+#if UNITY_EDITOR
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            PlayerPrefs.DeleteAll();
+        }
+#endif
+        if(isTimeUp){
+            isTimeUp = false;
+            StartCoroutine(OnWakeUp());
+        }
+    }
+
+    #endregion
+
+    #region PublicMethod
+    public void StartCountdown(){
+
+        GUIController.instance.DisableAll();
+        double resultCountdown = 0;
+        Double.TryParse(countdownInputField.text,out resultCountdown);
+        AlarmClock clock = new AlarmClock(DateTime.Now.AddSeconds(resultCountdown));
+        clock.Alarm += (sender, e) => Alarm();
+    }
+    public void Alarm()
+    {
+        Debug.Log("Wake up");
+        GUIController.instance.panelAlarm.SetActive(true);
+        isTimeUp = true;
+    }
+
+    IEnumerator OnWakeUp(){
+
+        while(true){
+            thalmicMyo.Vibrate(VibrationType.Short);
+            yield return new WaitForSeconds(0.5f);
+        }
+
+    }
+
+	#endregion
+
+}
